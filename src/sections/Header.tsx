aeed045066cb9ead35c33e58d7637a80c9ca6816
D_Zucho <danny.nico.dev@outlook.com>
2024-09-11 06:26:44 -0700

    add mobile nav skel

---
+++ src/sections/Header.tsx
@@ -0,0 +1,25 @@
+const Header = () => {
+  return (
+    <header className='sticky top-0 bg-zinc-900/50 backdrop-blur-lg'>
+      <div className='container'>
+        <div className='flex justify-between items-center h-24'>
+          <div>
+            <img src='/assets/images/logo.svg' alt='BlockForge' />
+          </div>
+          <div>
+            <div className='size-10 relative'>
+              <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <div className='w-5 h-0.5 bg-zinc-300 -translate-y-1'></div>
+              </div>
+              <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <div className='w-5 h-0.5 bg-zinc-300 translate-y-1'></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
